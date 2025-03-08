d44d2211a3cb819c77d08aa8e8fe1f53cf2da172
Rasmus <rraasu@taltech.ee>
2025-03-08 23:02:30 +0200

    finished Lab5

--- Labs/Lab5.lean
+++ Labs/Lab5.lean
@@ -19,7 +19,7 @@ instance : ToString Shape where
 -- Task 2
 instance [BEq α] : BEq (List α) where
   beq x y := List.all x (λ z => List.elem z y)
-  
+
 #eval [1, 2, 3] == [3, 2, 1]
 #eval [1, 2, 3] == [1, 2, 3, 3]
 #eval [1, 2, 3] == [1, 2, 3]
@@ -28,17 +28,20 @@ instance [BEq α] : BEq (List α) where
 
 -- Task 3
 class PreOrd (α : Type) where
-  le : α → α → Prop
-  refl : le a a
-  trans : le a b → le b c → le a c
+  leq : α → α → Bool
 
+instance divides : PreOrd Int where
+  leq x y :=
+    x % y == 0
+
+#eval PreOrd.leq (Int.ofNat 2) (Int.ofNat 2)
+#eval PreOrd.leq (Int.ofNat 12) (Int.ofNat 2)
 
 -- Task 4
 inductive AExpr (α : Type) : Type
 | V : α → AExpr α
 | Plus : AExpr α → AExpr α → AExpr α
 | Times : AExpr α → AExpr α → AExpr α
-
 
 def eval [Add α] [Mul α] (exp : AExpr α) : α :=
   match exp with
@@ -52,14 +55,17 @@ def eval [Add α] [Mul α] (exp : AExpr α) : α :=
     let b := eval y
     a * b
 
-instance [Add α] [Mul α] [LT α] [DecidableLT α] : Ord (AExpr α) where
-  compare x y :=
-  if eval x < eval y then
-    Ordering.lt
-  else if eval x > eval y then
-    Ordering.gt
-  else
-    Ordering.eq
+instance ord [Add α] [Mul α] [Ord α] : Ord (AExpr α) where
+  compare x y := compare (eval x) (eval y)
+
+instance [Ord (AExpr α)] : LE (AExpr α) := leOfOrd
+instance [Ord (AExpr α)] : LT (AExpr α) := ltOfOrd
+instance [Ord (AExpr α)] : Max (AExpr α) := maxOfLe
+
+open AExpr
+#eval (V 2) < (V 3)
+#eval (Plus (V 2) (V 1)) < (V 3)
+#eval  max (Plus (V 2) (V 4)) (V 3)
 
 
 -- Task 5
@@ -69,9 +75,18 @@ instance : Coe Bool Int where
     | .true => 1
     | .false => 0
 
+#eval (true : Int)
+#eval (false : Int)
+
 instance : Coe Int Bool where
   coe i :=
     match i with
+    | 0 => false
     | .ofNat _ => true
     | .negSucc _ => false
+
+#eval (Int.ofNat 5 : Bool)
+#eval (Int.ofNat 0  : Bool)
+#eval (Int.negOfNat 5  : Bool)
+
 
